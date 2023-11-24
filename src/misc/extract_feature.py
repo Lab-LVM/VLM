a52ed1671058b9157f2af92bd606024130afffb7
SoongE <oh1.seungmin@gmail.com>
2023-11-24 16:11:48 +0900

    Add feature extraction

---
+++ src/misc/extract_feature.py
@@ -0,0 +1,113 @@
+import torch
+from tqdm import tqdm
+
+from src.data import create_dataset
+from src.models import CLIP_tokenizer, CLIP
+
+template = [
+    'a bad photo of a {}.',
+    'a photo of many {}.',
+    'a sculpture of a {}.',
+    'a photo of the hard to see {}.',
+    'a low resolution photo of the {}.',
+    'a rendering of a {}.',
+    'graffiti of a {}.',
+    'a bad photo of the {}.',
+    'a cropped photo of the {}.',
+    'a tattoo of a {}.',
+    'the embroidered {}.',
+    'a photo of a hard to see {}.',
+    'a bright photo of a {}.',
+    'a photo of a clean {}.',
+    'a photo of a dirty {}.',
+    'a dark photo of the {}.',
+    'a drawing of a {}.',
+    'a photo of my {}.',
+    'the plastic {}.',
+    'a photo of the cool {}.',
+    'a close-up photo of a {}.',
+    'a black and white photo of the {}.',
+    'a painting of the {}.',
+    'a painting of a {}.',
+    'a pixelated photo of the {}.',
+    'a sculpture of the {}.',
+    'a bright photo of the {}.',
+    'a cropped photo of a {}.',
+    'a plastic {}.',
+    'a photo of the dirty {}.',
+    'a jpeg corrupted photo of a {}.',
+    'a blurry photo of the {}.',
+    'a photo of the {}.',
+    'a good photo of the {}.',
+    'a rendering of the {}.',
+    'a {} in a video game.',
+    'a photo of one {}.',
+    'a doodle of a {}.',
+    'a close-up photo of the {}.',
+    'a photo of a {}.',
+    'the origami {}.',
+    'the {} in a video game.',
+    'a sketch of a {}.',
+    'a doodle of the {}.',
+    'a origami {}.',
+    'a low resolution photo of a {}.',
+    'the toy {}.',
+    'a rendition of the {}.',
+    'a photo of the clean {}.',
+    'a photo of a large {}.',
+    'a rendition of a {}.',
+    'a photo of a nice {}.',
+    'a photo of a weird {}.',
+    'a blurry photo of a {}.',
+    'a cartoon {}.',
+    'art of a {}.',
+    'a sketch of the {}.',
+    'a embroidered {}.',
+    'a pixelated photo of a {}.',
+    'itap of the {}.',
+    'a jpeg corrupted photo of the {}.',
+    'a good photo of a {}.',
+    'a plushie {}.',
+    'a photo of the nice {}.',
+    'a photo of the small {}.',
+    'a photo of the weird {}.',
+    'the cartoon {}.',
+    'art of the {}.',
+    'a drawing of the {}.',
+    'a photo of the large {}.',
+    'a black and white photo of a {}.',
+    'the plushie {}.',
+    'a dark photo of a {}.',
+    'itap of a {}.',
+    'graffiti of the {}.',
+    'a toy {}.',
+    'itap of my {}.',
+    'a photo of a cool {}.',
+    'a photo of a small {}.',
+    'a tattoo of the {}.',
+]
+from hydra import initialize, compose
+
+with initialize('../../configs', version_base='1.3'):
+    cfg = compose('eval_config', overrides=['dataset=imagenet'])
+
+model, tokenizer = CLIP(), CLIP_tokenizer()
+dataset = create_dataset(cfg.dataset)
+
+device = torch.device('cuda:0')
+text_classifier = list()
+model.eval()
+model.to(device)
+
+for class_name in tqdm(dataset.class_name, desc='Build Text Classifier'):
+    class_name = class_name.replace('_', '')
+    text = [t.format(class_name) for t in template]
+    text_input = tokenizer(text).to(device)
+
+    text_feature = model.encode_text(text_input)
+    text_classifier.append(text_feature.detach().cpu())
+    # text_feature /= text_feature.norm(dim=-1, keepdim=True).mean(dim=0)
+    # text_feature /= text_feature.norm()
+
+text_classifier = torch.stack(text_classifier, dim=0)
+torch.save(text_classifier, 'text_feature_80.pt')
