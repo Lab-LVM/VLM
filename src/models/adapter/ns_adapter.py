6dc4695bc43ef34fddcc8dc9e59cfc87af2d916b
SoongE <oh1.seungmin@gmail.com>
2023-12-18 10:56:40 +0900

    Clean code

--- src/models/adapter/ns_adapter.py
+++ src/models/adapter/ns_adapter.py
@@ -3,7 +3,7 @@ from torch import nn
 
 import src.models.clip as clip
 from src.misc.metadata import format_bytes
-from src.models.adapter.ns_adapter_sub_block import SubClassifier, SubConvMLP1D, SubMLP
+from src.models.adapter.ns_adapter_sub_block import SubClassifier, SubMLP
 from src.utils.registry import register_model
 
 
@@ -29,7 +29,7 @@ def forward(self, image, text):
     return logits_per_image, logits_per_text, self.classifier(image_features)
 
 
-def mlp2():
+def mlp():
     return torch.nn.Sequential(
         torch.nn.Linear(512, 512 * 2),
         torch.nn.GELU(),
@@ -50,34 +50,6 @@ def deploy(model):
             module.re_parameterized = True
 
 
-@register_model  # Pick!
-def CLIP_NSAdapter2(backbone='ViT-B32', freeze=False, finetune=False, language_adapter=False, vision_adapter=False,
-                    classifier=False, **kwargs):
-    assert finetune
-    model, _ = clip.load(backbone)
-    delattr(model, 'transformer')
-    delattr(model, 'text_projection')
-    delattr(model, 'logit_scale')
-
-    if freeze:
-        for name, param in model.named_parameters():
-            param.requires_grad = False
-
-    if finetune:
-        assert language_adapter or vision_adapter
-        import torch
-
-        model.__setattr__('vision_adapter', SubMLP(512, n_blocks=5))
-        encode_image_bound_method = encode_image.__get__(model, model.__class__)
-        setattr(model, 'encode_image', encode_image_bound_method)
-
-        model.__setattr__('classifier', SubClassifier(512, kwargs['num_classes'], 5))
-        forward_bound_method = forward.__get__(model, model.__class__)
-        setattr(model, 'forward', forward_bound_method)
-
-    return model
-
-
 @register_model
 def CLIP_NSAdapter(backbone='ViT-B32', freeze=False, finetune=False, language_adapter=False, vision_adapter=False,
                    classifier=False, **kwargs):
@@ -95,7 +67,7 @@ def CLIP_NSAdapter(backbone='ViT-B32', freeze=False, finetune=False, language_ad
         assert language_adapter or vision_adapter
         import torch
 
-        model.__setattr__('vision_adapter', SubConvMLP1D(512, 512, n_blocks=5))
+        model.__setattr__('vision_adapter', SubMLP(512, n_blocks=5))
         encode_image_bound_method = encode_image.__get__(model, model.__class__)
         setattr(model, 'encode_image', encode_image_bound_method)
 
@@ -123,35 +95,7 @@ def CLIP_BaseAdapter(backbone='ViT-B32', freeze=False, finetune=False, language_
         assert language_adapter or vision_adapter
         import torch
 
-        model.__setattr__('vision_adapter', nn.Identity())
-        encode_image_bound_method = encode_image.__get__(model, model.__class__)
-        setattr(model, 'encode_image', encode_image_bound_method)
-
-        model.__setattr__('classifier', torch.nn.Linear(512, kwargs['num_classes']))
-        forward_bound_method = forward.__get__(model, model.__class__)
-        setattr(model, 'forward', forward_bound_method)
-
-    return model
-
-
-@register_model
-def CLIP_BaseAdapter2(backbone='ViT-B32', freeze=False, finetune=False, language_adapter=False, vision_adapter=False,
-                      classifier=False, **kwargs):
-    assert finetune
-    model, _ = clip.load(backbone)
-    delattr(model, 'transformer')
-    delattr(model, 'text_projection')
-    delattr(model, 'logit_scale')
-
-    if freeze:
-        for name, param in model.named_parameters():
-            param.requires_grad = False
-
-    if finetune:
-        assert language_adapter or vision_adapter
-        import torch
-
-        model.__setattr__('vision_adapter', mlp2())
+        model.__setattr__('vision_adapter', mlp())
         encode_image_bound_method = encode_image.__get__(model, model.__class__)
         setattr(model, 'encode_image', encode_image_bound_method)
 
