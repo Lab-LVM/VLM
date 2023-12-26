d424c9ae43911aa51e3873775ca2f3941d4fbea3
SoongE <oh1.seungmin@gmail.com>
2023-12-27 03:00:52 +0900

    Clean code

--- src/misc/train_feature_extractor.py
+++ src/misc/train_feature_extractor.py
@@ -7,12 +7,14 @@ from hydra import initialize, compose
 from tqdm import tqdm
 
 from src.data import create_dataloader
+from src.data.builder import create_transform
 from src.data.dataset import ImageNetRandaugPromptText
 from src.models import CLIP_tokenizer
 import src.models.clip as clip
 
+os.environ['TOKENIZERS_PARALLELISM'] = 'true'
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 
 def forward_for_feature_extraction(self, image, text):
@@ -21,7 +23,7 @@ def forward_for_feature_extraction(self, image, text):
     return image_features, text_features
 
 
-def CLIPTMP(backbone='ViT-B16'):
+def CLIPTMP(backbone):
     model, _ = clip.load(backbone)
 
     for name, param in model.named_parameters():
@@ -35,7 +37,7 @@ def CLIPTMP(backbone='ViT-B16'):
 
 def create_dataset(ds_cfg, **kwargs):
     ds_kwargs = dict(
-        transform=kwargs.get('transform', None),
+        transform=kwargs.get('transform', create_transform(ds_cfg, True)),
         root=kwargs.get('root', ds_cfg.root),
         target_transform=kwargs.get('target_transform', None),
         n_shot=kwargs.get('n_shot', 0),
@@ -47,10 +49,11 @@ def create_dataset(ds_cfg, **kwargs):
 
 
 if __name__ == '__main__':
-    with initialize('configs', version_base='1.3'):
-        cfg = compose('train_config', overrides=['model.backbone=ViT-B16', '+setup=our',
+    with initialize('../../configs', version_base='1.3'):
+        cfg = compose('train_config', overrides=['model.backbone=ViT-B32', '+setup=our',
                                                  'dataset.augmentation.prefetcher=False'])
     cfg.train.batch_size = 1024
+    print(cfg.model.backbone)
 
     device = torch.device('cuda')
 
@@ -60,32 +63,33 @@ if __name__ == '__main__':
 
     tokenizer = CLIP_tokenizer()
 
-    ds = create_dataset(cfg.dataset, split=cfg.dataset.train, n_shot=0)
+    ds = create_dataset(cfg.dataset, split=cfg.dataset.train, n_shot=0, is_train=True)
     dl = create_dataloader(cfg, ds, is_train=True)
     dl.dataset.setup_prompt_transform()
-    root = Path(f'/home/seungmin/dmount/feature_data/imageNet_train_features_B16_train')
+    root = Path(f'/home/seungmin/dmount/feature_data/B32_imageNet_train')
     root.mkdir(exist_ok=True)
 
     keys = ('vision_features', 'language_features', 'targets')
 
-    for i in range(10,11):
-        print(f'EPOCH: {i}')
-        obj = {k: list() for k in keys}
+    with torch.cuda.amp.autocast():
+        for i in range(0, 11):
+            print(f'EPOCH: {i}')
+            obj = {k: list() for k in keys}
 
-        with torch.no_grad():
-            for e, (x, y, prompt) in tqdm(enumerate(dl), total=len(dl)):
-                x = x.to(device)
-                y = y.to(device)
-                prompt = tokenizer(prompt, padding='max_length', return_attention_mask=False, return_tensors='pt')[
-                    'input_ids'].to(device)
+            with torch.no_grad():
+                for e, (x, y, prompt) in tqdm(enumerate(dl), total=len(dl)):
+                    x = x.to(device)
+                    y = y.to(device)
+                    prompt = tokenizer(prompt, padding='max_length', return_attention_mask=False, return_tensors='pt')[
+                        'input_ids'].to(device)
 
-                im, te = clip(x, prompt)
-                obj['vision_features'].append(im)
-                obj['language_features'].append(te)
-                obj['targets'].append(y)
+                    im, te = clip(x, prompt)
+                    obj['vision_features'].append(im)
+                    obj['language_features'].append(te)
+                    obj['targets'].append(y)
 
-        for k in keys:
-            obj[k] = torch.concat(obj[k], dim=0).detach().cpu()
+            for k in keys:
+                obj[k] = torch.concat(obj[k], dim=0).detach().cpu()
 
-        with open(root / f'{i}.pkl', 'wb') as f:
-            pickle.dump(obj, f)
+            with open(root / f'{i}.pkl', 'wb') as f:
+                pickle.dump(obj, f)
