03a73259a558e36f355638ffec0a803c14540892
SoongE <oh1.seungmin@gmail.com>
2024-01-05 00:36:16 +0900

    Add prompts

--- src/data/dataset/imagenet.py
+++ src/data/dataset/imagenet.py
@@ -29,13 +29,86 @@ class ImageNet(VLMDataset, Dataset):
     @property
     def prompt(self):
         return [
-            lambda c: f'itap of a {c}.',
+            lambda c: f'a bad photo of a {c}.',
+            lambda c: f'a photo of many {c}.',
+            lambda c: f'a sculpture of a {c}.',
+            lambda c: f'a photo of the hard to see {c}.',
+            lambda c: f'a low resolution photo of the {c}.',
+            lambda c: f'a rendering of a {c}.',
+            lambda c: f'graffiti of a {c}.',
             lambda c: f'a bad photo of the {c}.',
-            lambda c: f'a origami {c}.',
-            lambda c: f'a photo of the large {c}.',
+            lambda c: f'a cropped photo of the {c}.',
+            lambda c: f'a tattoo of a {c}.',
+            lambda c: f'the embroidered {c}.',
+            lambda c: f'a photo of a hard to see {c}.',
+            lambda c: f'a bright photo of a {c}.',
+            lambda c: f'a photo of a clean {c}.',
+            lambda c: f'a photo of a dirty {c}.',
+            lambda c: f'a dark photo of the {c}.',
+            lambda c: f'a drawing of a {c}.',
+            lambda c: f'a photo of my {c}.',
+            lambda c: f'the plastic {c}.',
+            lambda c: f'a photo of the cool {c}.',
+            lambda c: f'a close-up photo of a {c}.',
+            lambda c: f'a black and white photo of the {c}.',
+            lambda c: f'a painting of the {c}.',
+            lambda c: f'a painting of a {c}.',
+            lambda c: f'a pixelated photo of the {c}.',
+            lambda c: f'a sculpture of the {c}.',
+            lambda c: f'a bright photo of the {c}.',
+            lambda c: f'a cropped photo of a {c}.',
+            lambda c: f'a plastic {c}.',
+            lambda c: f'a photo of the dirty {c}.',
+            lambda c: f'a jpeg corrupted photo of a {c}.',
+            lambda c: f'a blurry photo of the {c}.',
+            lambda c: f'a photo of the {c}.',
+            lambda c: f'a good photo of the {c}.',
+            lambda c: f'a rendering of the {c}.',
             lambda c: f'a {c} in a video game.',
-            lambda c: f'art of the {c}.',
+            lambda c: f'a photo of one {c}.',
+            lambda c: f'a doodle of a {c}.',
+            lambda c: f'a close-up photo of the {c}.',
+            lambda c: f'a photo of a {c}.',
+            lambda c: f'the origami {c}.',
+            lambda c: f'the {c} in a video game.',
+            lambda c: f'a sketch of a {c}.',
+            lambda c: f'a doodle of the {c}.',
+            lambda c: f'a origami {c}.',
+            lambda c: f'a low resolution photo of a {c}.',
+            lambda c: f'the toy {c}.',
+            lambda c: f'a rendition of the {c}.',
+            lambda c: f'a photo of the clean {c}.',
+            lambda c: f'a photo of a large {c}.',
+            lambda c: f'a rendition of a {c}.',
+            lambda c: f'a photo of a nice {c}.',
+            lambda c: f'a photo of a weird {c}.',
+            lambda c: f'a blurry photo of a {c}.',
+            lambda c: f'a cartoon {c}.',
+            lambda c: f'art of a {c}.',
+            lambda c: f'a sketch of the {c}.',
+            lambda c: f'a embroidered {c}.',
+            lambda c: f'a pixelated photo of a {c}.',
+            lambda c: f'itap of the {c}.',
+            lambda c: f'a jpeg corrupted photo of the {c}.',
+            lambda c: f'a good photo of a {c}.',
+            lambda c: f'a plushie {c}.',
+            lambda c: f'a photo of the nice {c}.',
             lambda c: f'a photo of the small {c}.',
+            lambda c: f'a photo of the weird {c}.',
+            lambda c: f'the cartoon {c}.',
+            lambda c: f'art of the {c}.',
+            lambda c: f'a drawing of the {c}.',
+            lambda c: f'a photo of the large {c}.',
+            lambda c: f'a black and white photo of a {c}.',
+            lambda c: f'the plushie {c}.',
+            lambda c: f'a dark photo of a {c}.',
+            lambda c: f'itap of a {c}.',
+            lambda c: f'graffiti of the {c}.',
+            lambda c: f'a toy {c}.',
+            lambda c: f'itap of my {c}.',
+            lambda c: f'a photo of a cool {c}.',
+            lambda c: f'a photo of a small {c}.',
+            lambda c: f'a tattoo of the {c}.',
         ]
 
     def _data_dict(self):
@@ -424,14 +497,15 @@ class ImageNetSimplePrompt(ImageNet):
         pass
 
     def ra_prompt(self, target):
-        return [49406, 320, 3117, 1125, 539, 518] + IMAGENET_CLASS_NAME_TOKEN[target] + [269, 49407]
+        prompt = [49406, 320, 3117, 1125, 539, 518] + IMAGENET_CLASS_NAME_TOKEN[target] + [269]
+        return prompt + [49407] * (MAX_LENGTH - len(prompt))
 
     def __getitem__(self, idx):
         path, target = self.imgs[idx], self.targets[idx]
         img = self.loader(path)
         img = self.transform(img)
 
-        return img, target, self.ra_prompt(target)
+        return img, target, torch.tensor(self.ra_prompt(target), dtype=torch.int64)
 
 
 if __name__ == '__main__':
