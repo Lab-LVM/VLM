8e784a47aa087d9ada00c3676015bc451f406650
SoongE <oh1.seungmin@gmail.com>
2024-01-16 21:36:51 +0900

    Add wild data

--- run_eval_full.py
+++ run_eval_full.py
@@ -31,10 +31,7 @@ def main(cfg: DictConfig) -> None:
             train_dataset = create_dataset(cfg.dataset, is_train=True, split=cfg.dataset.train)
             test_dataset = create_dataset(cfg.dataset, is_train=False, split=cfg.dataset.test)
 
-            if 'our' == cfg.model.model_name.lower():
-                engine = OurFullyTaskEngine(cfg, fabric, model, tokenizer, train_dataset, test_dataset)
-            else:
-                engine = create_task_engine(cfg, fabric, model, tokenizer, train_dataset, test_dataset)
+            engine = create_task_engine(cfg, fabric, model, tokenizer, train_dataset, test_dataset)
             metrics = engine(n_shots=to_list(cfg.n_shot))
 
             row = dict(Data=test_dataset.name, shot=shot, **metrics)
