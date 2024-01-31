a4319eaf1424b041bbf519ac52f003a6c97f1c21
SoongE <oh1.seungmin@gmail.com>
2024-01-31 22:25:37 +0900

    Update

---
+++ sh_inference_epoch.sh
@@ -0,0 +1,37 @@
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_1.ckpt dataset.name=imagenet gpu=9 name=OurE1
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_2.ckpt dataset.name=imagenet gpu=9 name=OurE2
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_3.ckpt dataset.name=imagenet gpu=9 name=OurE3
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_4.ckpt dataset.name=imagenet gpu=9 name=OurE4
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_5.ckpt dataset.name=imagenet gpu=9 name=OurE5
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_6.ckpt dataset.name=imagenet gpu=9 name=OurE6
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_7.ckpt dataset.name=imagenet gpu=9 name=OurE7
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_8.ckpt dataset.name=imagenet gpu=9 name=OurE8
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_9.ckpt dataset.name=imagenet gpu=9 name=OurE9
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_10.ckpt dataset.name=imagenet gpu=9 name=OurE10
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_11.ckpt dataset.name=imagenet gpu=9 name=OurE11
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_12.ckpt dataset.name=imagenet gpu=9 name=OurE12
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_13.ckpt dataset.name=imagenet gpu=9 name=OurE13
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_14.ckpt dataset.name=imagenet gpu=9 name=OurE14
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=512 checkpoint=finetune/B16_E15/e_15.ckpt dataset.name=imagenet gpu=9 name=OurE15
+
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_1.ckpt dataset.name=imagenet gpu=9 name=SoftCEE1
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_2.ckpt dataset.name=imagenet gpu=9 name=SoftCEE2
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_3.ckpt dataset.name=imagenet gpu=9 name=SoftCEE3
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_4.ckpt dataset.name=imagenet gpu=9 name=SoftCEE4
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_5.ckpt dataset.name=imagenet gpu=9 name=SoftCEE5
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_6.ckpt dataset.name=imagenet gpu=9 name=SoftCEE6
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_7.ckpt dataset.name=imagenet gpu=9 name=SoftCEE7
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_8.ckpt dataset.name=imagenet gpu=9 name=SoftCEE8
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_9.ckpt dataset.name=imagenet gpu=9 name=SoftCEE9
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_10.ckpt dataset.name=imagenet gpu=9 name=SoftCEE10
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_11.ckpt dataset.name=imagenet gpu=9 name=SoftCEE11
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_12.ckpt dataset.name=imagenet gpu=9 name=SoftCEE12
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_13.ckpt dataset.name=imagenet gpu=9 name=SoftCEE13
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_14.ckpt dataset.name=imagenet gpu=9 name=SoftCEE14
+pt run_eval_full.py +setup=our2 train.num_workers=8 train.batch_size=4096 checkpoint=finetune/B16_E15_SoftCE/e_15.ckpt dataset.name=imagenet gpu=9 name=SoftCEE15
+
+
+
+pt run_eval_full.py +setup=our2 model.finetune=False train.num_workers=4 checkpoint=ARCHIVE_WEIGHTS/B16/flyp_B16_e15.ckpt dataset.name=imagenet_ds gpu=7 name=FLYP_E15
+pt run_eval_full.py +setup=clip model.classifier=True train.num_workers=4 checkpoint=ARCHIVE_WEIGHTS/B16/WiseFT_B16_e15.ckpt dataset.name=imagenet_ds gpu=9 name=WiseFT_E15
+pt run_eval_full.py +setup=lp train.num_workers=4 checkpoint=ARCHIVE_WEIGHTS/B16/FT_B16_e15.ckpt dataset.name=imagenet_ds gpu=8 name=FFT_E15
