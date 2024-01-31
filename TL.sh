9e8b94dfbf3688051597fb6e12a0f95a67741f86
SoongE <oh1.seungmin@gmail.com>
2024-01-31 22:25:40 +0900

    Update

--- TL.sh
+++ TL.sh
@@ -1,14 +1,15 @@
 export _DS_NAME=flowers102
 python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0 model.finetune=False name=Flower102_noAdapter
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} name=Flower102_noWD
-
-export _DS_NAME=cifar100
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0 model.finetune=False name=CIFAR100_noAdapter
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} name=CIFAR100_noWD
 
 export _DS_NAME=pcam
 python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0 model.finetune=False name=PCam_noAdapter
-python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} name=PCam_noWD
+
+export _DS_NAME=caltech101
+python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0
+
+export _DS_NAME=stanfordcars
+python main_full.py +setup=our2 train.epochs=100 dataset=${_DS_NAME} dataset.name=${_DS_NAME}ra dataset@eval_dataset=${_DS_NAME} train.optimizer.weight_decay=0.0
+
+python main_full.py +setup=our2_iwild dataset=iwildcam dataset.name=iwildcamra dataset@eval_dataset=iwildcam train.epochs=20 name=iWilCam_E20
+#python main_full.py +setup=our2_iwild dataset=iwildcam dataset.name=iwildcamra dataset@eval_dataset=iwildcam train.epochs=50 name=iWilCam_E50
+#python main_full.py +setup=our2_iwild dataset=iwildcam dataset.name=iwildcamra dataset@eval_dataset=iwildcam train.epochs=40 name=iWilCam_E40
